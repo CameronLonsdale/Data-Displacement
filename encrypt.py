f8d35d93d1bbcbcdc5074fb5483ea854c25fb560
CLonsdale <cameron.lonsdale@gmail.com>
2015-08-30 12:52:22 +1000

    Removed input prompts for easy terminal execution Updated sytax to python 3 Changed calc_pi() to yield forever

--- encrypt.py
+++ encrypt.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
+
 from pi_extension import calcPi
 import string
 import random
 
 def encrypt() :
-    message = raw_input("Enter the message : ")
+    message = input()
     message_length = len(message)
 
     message_index = 0
     encrypt_message = []
     
-    for digit in calcPi(message_length-1):
+    for digit in calcPi():
+        if message_index > message_length-1 : break
         index = 0
         while (index < digit) :
             encrypt_message.append(random.choice(string.printable))
@@ -18,6 +20,6 @@ def encrypt() :
         encrypt_message.append(message[message_index])
         message_index += 1
 
-    print ''.join(encrypt_message)
+    print (''.join(encrypt_message))
 
 encrypt()
--- pi_extension.py
+++ pi_extension.py
@@ -1,25 +1,22 @@
 # Following Code to calculate digits of Pi was written by 
 #"Balazs Rostas : MrBlaise"
 #https://github.com/MrBlaise/learnpython/blob/master/Numbers/pi.py
+# Modified by Cameron Lonsdale
 
 # Find PI to the Nth Digit
 # Have the user enter a number 'n'
 # and print out PI to the 'n'th digit
 
-def calcPi(limit):  # Generator function
+def calcPi():  # Generator function
     #Prints out the digits of PI
     #until it reaches the given limit
 
     q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
 
-    decimal = limit
-    counter = 0
-
-    while counter != decimal + 1:
+    while True:
         if 4 * q + r - t < n * t:
             # yield digit
             yield n
-            counter += 1
             nr = 10 * (r - n * t)
             n = ((10 * (3 * q + r)) // t) - 10 * n
             q *= 10
