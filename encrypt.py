6a100d0e68508ead3a264b08e4b84fbf6dd9aec1
CLonsdale <cameron.lonsdale@gmail.com>
2015-11-19 13:31:36 +1100

    setup for line by line encryption and generator fixes

--- encrypt.py
+++ encrypt.py
@@ -1,28 +1,41 @@
 #!/usr/bin/env python3
 
 from pi_extension import calcPi
-import string
-import random
+import string, random, sys
 
-# no new line support as of yet
+# Main
 printables = string.ascii_uppercase + string.digits + " " + string.punctuation
 
-def encrypt() :
-    message = input()
-    message_length = len(message)
+for arg in sys.argv:
+    if arg == '-e':
+        # Use irrational e as key
+        key = 'e'
+    elif arg == '-m':
+        # Use the ascii message as key
+        key = 'm'
+    else:
+        # use Pi as key
+        key = calcPi()
 
-    message_index = 0
-    encrypt_message = []
+def encrypt():
+    message = sys.stdin.readlines()
     
-    for digit in calcPi():
-        if message_index > message_length-1 : break
-        index = 0
-        while (index < digit) :
-            encrypt_message.append(random.choice(printables))
-            index += 1
-        encrypt_message.append(message[message_index])
-        message_index += 1
+    # line by line encryption
+    for line in message:
+        message_length = len(line)
+        message_index = 0
+        encrypt_message = []
 
-    print (''.join(encrypt_message))
+        # loop through key
+        for digit in key:
+            if message_index > message_length-1 : break
+            index = 0
+            while (index < digit) :
+                encrypt_message.append(random.choice(printables))
+                index += 1
+            encrypt_message.append(line[message_index])
+            message_index += 1
+
+        sys.stdout.write(''.join(encrypt_message))
 
 encrypt()
