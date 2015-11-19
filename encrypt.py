d41025d2836ea622945bee4db4d76282dd92f5cf
CLonsdale <cameron.lonsdale@gmail.com>
2015-11-19 18:33:08 +1100

    updating syntax to conform with style guides

--- encrypt.py
+++ encrypt.py
@@ -1,59 +1,73 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-from pi_extension import calcPi
-import string, random, sys
+""" Mask or Unmask text from STDIN using the displacement method"""
+
+import sys
+import string
+import random
 import argparse
+from pi_extension import calc_pi
 
 def main():
-    # Command Line Arguments
-    parser = argparse.ArgumentParser(description='Encrypt/Decrypt using the Data-Displacement method.')
-    parser.add_argument('action', type=str, choices=['E', 'D'], help='E for encryption\n or D for decryption')
-    #parser.add_argument('-key', type=str, choices=['p', 'e', 'c'], help='Key to use')
-    args = parser.parse_args()
- 
-    key = calcPi()
-    if args.action == 'E': encrypt(key)
-    else: decrypt(key)
+    """ Evaluate arguments and either Mask or Unmask"""
 
-def encrypt(key):
-    message = sys.stdin.readlines()
+    # Command Line Arguments
+    parser = argparse.ArgumentParser(
+        description='Data Masking using Displacement.')
+    parser.add_argument('action', type=str, choices=['E', 'D'],
+        help='E for encryption\n or D for decryption')
+
+    args = parser.parse_args()
+    key = calc_pi()
+
+    if args.action == 'E':
+        mask(key)
+    else: 
+        unmask(key)
+
+
+def mask(key):
+    """ apply mask on stdin line by line and print"""
 
     # line by line encryption
-    for line in message:
-        message_length = len(line)
-        message_index = 0
+    for line in sys.stdin.readlines():
+        length = len(line)
+        index = 0
         encrypt_message = []
 
         # loop through key
         for digit in key:
-            if message_index > message_length-1 : break
-            index = 0
-            while (index < digit) :
-                encrypt_message.append(random.choice(printables))
-                index += 1
-            encrypt_message.append(line[message_index])
-            message_index += 1
+            if index > length-1: break
+            # Append dummy letters
+            for i in range(0, digit):
+                encrypt_message.append(random.choice(PRINTABLES))
+
+            # Append letter from message
+            encrypt_message.append(line[index])
+            index += 1
 
         sys.stdout.write(''.join(encrypt_message))
 
-def decrypt(key) :
-    message = sys.stdin.readlines()
+
+def unmask(key):
+    """ unmask on stdin line by line and print"""
 
     # line by line decryption
-    for line in message:
-        message_length = len(line)
-        message_index = -1
+    for line in sys.stdin.readlines():
+        length = len(line)
+        index = -1
         plain_text = []
 
         # loop through key
         for digit in key:
-            message_index += digit+1
-            if message_index > message_length-1 : break
-            plain_text.append(line[message_index])
-            
+            index += digit+1
+            if index > length-1: break
+            plain_text.append(line[index])
+
         sys.stdout.write(''.join(plain_text))
 
+
 # Main
-printables = string.ascii_uppercase + string.digits + " " + string.punctuation
+PRINTABLES = string.ascii_uppercase + string.digits + " " + string.punctuation
 if __name__ == '__main__':
     main()
