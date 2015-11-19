3dbbd30f45511dbdf1b21b5779f7254e43d61b80
CLonsdale <cameron.lonsdale@gmail.com>
2015-11-19 13:41:13 +1100

    decrypt adjusted to accomodate encrypt changes

--- decrypt.py
+++ decrypt.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 
 from pi_extension import calcPi
-import string
-import random
+import string, random, sys
+
+key = calcPi()
 
 def decrypt() :
-    message = input()
+    message = sys.stdin.readlines()
 
-    message_length = len(message)
-    message_index = -1
-    plain_text = []
+    # line by line decryption
+    for line in message:
+        message_length = len(line)
+        message_index = -1
+        plain_text = []
 
-    for digit in calcPi():
-        message_index += digit+1
-        if message_index > message_length-1 : break
-        plain_text.append(message[message_index])
-        
-    print (''.join(plain_text))
+        # loop through key
+        for digit in key:
+            message_index += digit+1
+            if message_index > message_length-1 : break
+            plain_text.append(line[message_index])
+            
+        sys.stdout.write(''.join(plain_text))
 
 decrypt()
