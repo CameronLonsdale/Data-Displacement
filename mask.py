d8b5c69942492ad0901b5a79d76f8b007a60ff1c
CLonsdale <cameron.lonsdale@gmail.com>
2015-11-19 19:56:18 +1100

    Docstring and small code changes

--- mask.py
+++ mask.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Mask or Unmask text from STDIN using the displacement method
+"""Mask or Unmask text from STDIN using the displacement method"""
 
 import sys
 import string
@@ -8,9 +8,9 @@ import random
 import argparse
 from pi_extension import calc_pi
 
-# Evaluate arguments and either Mask or Unmask
-# 
 def main():
+    """Evaluate arguments and either Mask or Unmask."""
+
     # Command Line Arguments
     parser = argparse.ArgumentParser(
         description='Data Masking using Displacement.')
@@ -25,43 +25,40 @@ def main():
     else: 
         unmask(key)
 
-# Apply mask on STDIN line by line and print
-#
 def mask(key):
+    """Apply mask on STDIN line by line and print."""
+    
     # Line by line masking
     for line in sys.stdin.readlines():
-        length = len(line)
-        line_index = 0
-        encrypt_message = []
+        encrypt_message = ""
 
-        for digit in key:
-            if line_index > length-1: break
+        for char, digit in zip(line, key):
             # Append dummy letters
-            for i in range(0, digit):
-                encrypt_message.append(random.choice(PRINTABLES))
+            for i in range(digit):
+                encrypt_message += random.choice(PRINTABLES)
 
             # Append letter from plain text
-            encrypt_message.append(line[line_index])
-            line_index += 1
+            encrypt_message += char
 
-        sys.stdout.write(''.join(encrypt_message))
+        print(encrypt_message, end="")
 
-# Unmask on STDIN line by line and print
-#
+
 def unmask(key):
+    """Unmask on STDIN line by line and print."""
+
     # Line by line unmasking
     for line in sys.stdin.readlines():
-        length = len(line)
         line_index = -1
-        plain_text = []
+        plain_text = ""
 
         for digit in key:
             # Extract correct character
-            line_index += digit+1
-            if line_index > length-1: break
-            plain_text.append(line[line_index])
+            line_index += digit + 1
+            if line_index > len(line) - 1: break
+            plain_text += line[line_index]
 
-        sys.stdout.write(''.join(plain_text))
+        print(plain_text, end="")
+
 
 PRINTABLES = string.ascii_letters + string.digits + " " + string.punctuation
 if __name__ == '__main__':
